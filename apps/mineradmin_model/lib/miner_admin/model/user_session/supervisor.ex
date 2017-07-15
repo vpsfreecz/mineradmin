0a1aa8e86b9a50dc671488ec77581b4c123f3648
Jakub Skokan <jakub.skokan@havefun.cz>
2017-07-15 18:26:44 +0200

    UserSession: remove unneeded Starter module

--- apps/mineradmin_model/lib/miner_admin/model/user_session/supervisor.ex
+++ apps/mineradmin_model/lib/miner_admin/model/user_session/supervisor.ex
@@ -1,17 +1,34 @@
 defmodule MinerAdmin.Model.UserSession.Supervisor do
   use Supervisor
   alias MinerAdmin.Model
+  require Logger
 
   def start_link do
     Supervisor.start_link(__MODULE__, [], name: __MODULE__)
   end
 
   def init([]) do
-    children = [
-      supervisor(Model.UserSession.WorkerSupervisor, []),
-      worker(Model.UserSession.Starter, [], restart: :transient),
-    ]
+    supervise(active_sessions(), strategy: :one_for_one, name: __MODULE__)
+  end
 
-    supervise(children, strategy: :rest_for_one, name: __MODULE__)
+  def add_session(session) do
+    Supervisor.start_child(__MODULE__, worker_spec(session))
+  end
+
+  defp active_sessions do
+    for s <- Model.Query.UserSession.active_sessions do
+      Logger.debug "Monitoring user session #{s.id}"
+
+      worker_spec(s)
+    end
+  end
+
+  defp worker_spec(session) do
+    worker(
+      Model.UserSession,
+      [session, [name: Model.UserSession.via_tuple(session)]],
+      restart: :transient,
+      id: session.id
+    )
   end
 end
