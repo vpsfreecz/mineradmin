4282c6601a1b5cd3d4905b2289193ab684c08a8f
Jakub Skokan <jakub.skokan@havefun.cz>
2017-07-17 18:53:16 +0200

    Api: add functions to convert between Ecto and HaveAPI associations

---
+++ apps/mineradmin_api/lib/miner_admin/api.ex
@@ -0,0 +1,44 @@
+defmodule MinerAdmin.Api do
+  @doc """
+  Replace Ecto associations with values accepted by HaveAPI as output parameters.
+  """
+  def resourcify(list, associations) when is_list(list) do
+    Enum.map(list, &(resourcify(&1, associations)))
+  end
+
+  def resourcify(struct, associations) when is_map(struct) do
+    Enum.reduce(
+      associations,
+      struct,
+      fn assoc, acc ->
+        %{acc | assoc => assoc_to_resource(Map.fetch!(struct, :"#{assoc}_id"))}
+      end
+    )
+  end
+
+  @ doc """
+  Convert HaveAPI resource input parameters into fields understood by Ecto.
+  """
+  def associatify(data, associations) when is_map(data) do
+    Enum.reduce(
+      associations,
+      data,
+      fn assoc, acc ->
+        resource_to_assoc(acc, assoc)
+      end
+    )
+  end
+
+  def assoc_to_resource(nil), do: nil
+  def assoc_to_resource(id), do: [id]
+
+  def resource_to_assoc(data, assoc) do
+    if Map.has_key?(data, assoc) do
+      data = Map.put(data, :"#{assoc}_id", Map.fetch!(data, assoc))
+      Map.delete(data, assoc)
+
+    else
+      data
+    end
+  end
+end
