2e0027bc6c203f264cf117335a0e2f55697bd4c8
Jakub Skokan <jakub.skokan@havefun.cz>
2017-07-31 10:57:47 +0200

    Add initial support for nheqminer

---
+++ core/apps/mineradmin_base/lib/miner_admin/base/program/nheqminer.ex
@@ -0,0 +1,52 @@
+defmodule MinerAdmin.Base.Program.Nheqminer do
+  alias MinerAdmin.Base
+  alias MinerAdmin.Miner
+
+  @behaviour Base.Program
+
+  def changeset(changeset, _type) do
+    Ecto.Changeset.validate_change(changeset, :cmdline, fn :cmdline, cmdline ->
+      args = Base.UserProgram.arguments(cmdline)
+      errors = Enum.reduce(
+        ~w(-t -cd),
+        [],
+        fn v, acc ->
+          if Enum.find(args, nil, &(&1 == v)) do
+            ["must not contain option #{v}" | acc]
+          else
+            acc
+          end
+        end
+      )
+
+      if length(errors) > 0 do
+        [cmdline: Enum.join(errors, "; ")]
+
+      else
+        []
+      end
+    end)
+  end
+
+  def can_start?(user_prog) do
+    if Base.UserProgram.has_gpus?(user_prog) do
+      true
+
+    else
+      {:error, "Assign one or more GPUs"}
+    end
+  end
+
+  def command(user_prog) do
+    args = user_prog
+      |> Base.UserProgram.arguments()
+
+    gpus = user_prog
+      |> Base.Query.UserProgram.gpus()
+      |> Enum.map(&(&1.uuid))
+      |> Miner.GpuMapper.uuids_to_indexes()
+      |> Enum.join(",")
+
+    {"nheqminer", ["-cd", gpus] ++ args}
+  end
+end
