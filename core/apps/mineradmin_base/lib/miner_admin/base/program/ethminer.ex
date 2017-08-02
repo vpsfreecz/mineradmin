dab07429796b5f749314d818ce8ac54391d11ba7
Jakub Skokan <jakub.skokan@havefun.cz>
2017-08-02 08:52:22 +0200

    Add initial support for ethminer

---
+++ core/apps/mineradmin_base/lib/miner_admin/base/program/ethminer.ex
@@ -0,0 +1,39 @@
+defmodule MinerAdmin.Base.Program.Ethminer do
+  alias MinerAdmin.Base
+  alias MinerAdmin.Miner
+
+  @behaviour Base.Program
+
+  def changeset(changeset, _type) do
+    Base.UserProgram.exclude_arguments(changeset, ~w(
+      --cuda-devices
+      -G --opencl
+      -X --cuda-opencl
+      --opencl-device --opencl-devices
+      -t --mining-threads
+      --list-devices
+    ))
+  end
+
+  def can_start?(user_prog) do
+    if Base.UserProgram.has_gpus?(user_prog) do
+      true
+
+    else
+      {:error, "Assign one or more GPUs"}
+    end
+  end
+
+  def command(user_prog) do
+    args = user_prog
+      |> Base.UserProgram.arguments()
+
+    gpus = user_prog
+      |> Base.Query.UserProgram.gpus()
+      |> Enum.map(&(&1.uuid))
+      |> Miner.GpuMapper.uuids_to_indexes()
+      |> Enum.map(&to_string/1)
+
+    {"ethminer", args ++ ["-U", "--cuda-devices"] ++ gpus}
+  end
+end
