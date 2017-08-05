aa1161a9cdeecd1ca72e584bc3e8f672dabb80c6
Jakub Skokan <jakub.skokan@havefun.cz>
2017-08-05 17:38:55 +0200

    cluster-release: config file contains env and cookie

--- cluster-release.rb
+++ cluster-release.rb
@@ -5,20 +5,24 @@
 # Example `cluster.json` file:
 #
 #   {
-#     "all": [
-#       "node1@192.168.122.1"
-#     ],
-#     "worker": [
-#       "node2@192.168.122.2",
-#       "node2@192.168.122.3",
-#       "node2@192.168.122.4"
-#     ]
+#     "env": "prod",
+#     "cookie": "my-cookie",
+#     "nodes": {
+#       "all": [
+#         "node1@192.168.122.1"
+#       ],
+#       "worker": [
+#         "node2@192.168.122.2",
+#         "node2@192.168.122.3",
+#         "node2@192.168.122.4"
+#       ]
+#     }
 #   }
 #
 # Object keys are release types (see `core/rel/config.exs`), values are
 # arrays of node names. Built releases can be found at `releases/<env>/`.
 #
-# TODO: remote the need for sudo make clean (docker writes to the mounted
+# TODO: remove the need for sudo make clean (docker writes to the mounted
 #       volume as root, so only root can delete it)
 
 require 'json'
@@ -35,31 +39,21 @@ def cmd(s)
 end
 
 def run
-  options = {
-    env: 'prod',
-  }
+  cluster = JSON.parse(File.read(ARGV[0] || 'cluster.json'), symbolize_names: true)
 
-  OptionParser.new do |opts|
-    opts.on('-e', '--env ENV', 'Build environment') do |v|
-      options[:env] = v
-    end
-  end.parse!
-
-  cluster = JSON.parse(File.read(ARGV[0] || 'cluster.json'))
-
-  cluster.each do |role, nodes|
+  cluster[:nodes].each do |role, nodes|
     puts "Building core role #{role}"
     cmd("sudo make clean")
-    cmd("make ENV=#{options[:env]} build_core")
+    cmd("make ENV=#{cluster[:env]} build_core")
 
     nodes.each do |node|
       puts "Building node #{node} (#{role})"
-      cmd("make ENV=#{options[:env]} COOKIE=#{ENV['COOKIE']} NODE=#{node} TYPE=#{role} release_core")
+      cmd("make ENV=#{cluster[:env]} COOKIE=#{cluster[:cookie]} NODE=#{node} TYPE=#{role} release_core")
     end
   end
 
   puts "Building gems"
-  cmd("make ENV=#{options[:env]} mineradmin-client minerd minerdctl")
+  cmd("make ENV=#{cluster[:env]} mineradmin-client minerd minerdctl")
 end
 
 run
